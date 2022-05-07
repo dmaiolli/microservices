297539e933fc59be8d7a0048c88eef33bce46867
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2022-05-07 12:11:44 -0300

    Adding a listener to notification queue

---
+++ notification/src/main/java/br/com/denys/notification/rabbitmq/NotificationConsumer.java
@@ -0,0 +1,25 @@
+package br.com.denys.notification.rabbitmq;
+
+import br.com.denys.clients.notification.NotificationRequest;
+import br.com.denys.notification.NotificationService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+
+    private final NotificationService notificationService;
+
+    @RabbitListener(queues = "${rabbitmq.queues.notification}")
+    public void consumer(NotificationRequest notificationRequest) {
+        log.info("Consumed {} from queue", notificationRequest);
+        notificationService.send(notificationRequest);
+    }
+
+}
+
+
