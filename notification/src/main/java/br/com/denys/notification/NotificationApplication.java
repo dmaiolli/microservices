c11523d11871c474e2928896490cc410cfaf1879
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2022-05-06 22:08:33 -0300

    Implementing rabbitmq on notification ms to receive mesasages

--- notification/src/main/java/br/com/denys/notification/NotificationApplication.java
+++ notification/src/main/java/br/com/denys/notification/NotificationApplication.java
@@ -1,13 +1,19 @@
 package br.com.denys.notification;
 
+import br.com.denys.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {"br.com.denys.amqp", "br.com.denys.notification"}
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
 }
